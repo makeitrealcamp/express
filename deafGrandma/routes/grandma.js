4d30d3bc2132f8749bee8f6b678ac58a04fe051f
German Hernandez <muygrafico@gmail.com>
2016-09-30 14:51:32 -0500

    deaf grandma

--- deafGrandma/routes/grandma.js
+++ deafGrandma/routes/grandma.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+function isUpperCase(str) {
+    return str === str.toUpperCase();
+}
+
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  res.send(`Implementa esta ruta tu mismo <br /> Params: ${JSON.stringify(req.body)}`);
+
+  var grandma = req.body.userInput;
+  if(grandma){
+    switch (isUpperCase(grandma)) {
+      case true:
+          grandma = 'DO\'\nNT YELL!!!' + grandma;
+      break;
+      case false:
+          grandma = 'speak louder!' + grandma;
+      break;
+    }
+  }
+  res.render('index', { grandma: grandma});
+
 });
 
 module.exports = router;
+
+
+;
