f2d33bdd9d99b68ae94a17abf91e54bb4f246e6f
German Hernandez <muygrafico@gmail.com>
2016-10-03 10:50:34 -0500

    basic server

--- CRUD-GUI/app/server.js
+++ CRUD-GUI/app/server.js
@@ -1,1 +1,11 @@
 express = require('express')
+var app = express()
+var port = 3000
+
+app.get('/', function (req, res) {
+  res.send('server test')
+})
+
+app.listen(port, function () {
+  console.log(`listening ${port}`)
+})
