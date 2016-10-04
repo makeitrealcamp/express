670d249efa44e13c0a2e31e8eb6b1488b31854c5
upgraded-robot <mariadotdev.ln@gmail.com>
2016-10-04 10:54:59 -0500

    Finish deafGrandma and CRUD ui

--- deafGrandma/routes/grandma.js
+++ deafGrandma/routes/grandma.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
+/*router.post('/', function(req, res, next) {
   res.send(`Implementa esta ruta tu mismo <br /> Params: ${JSON.stringify(req.body)}`);
 });
+*/
+router.post('/', function(req, res) {
+  var userInput = req.body.userInput
+  var grandmaSays = "Mijo, cierre la ventana que se sale el wifi!"
+  var grandmaYells = "¡Hable más duro!"
+  if(isUpperCase(userInput)){
+	  	res.render('index',{
+	    grandma: grandmaSays,
+	  })
+  }else {
+  	res.render('index',{
+	    grandma: grandmaYells,
+	  })
+  	}
+})
+
+
+function isUpperCase(str) { 
+  return (str == str.toUpperCase()); 
+}
 
 module.exports = router;
